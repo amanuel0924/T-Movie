3fb04d9fb2c6b3129ffd1edb901cf6af115086b6
amanuel tegegne <amanuel0924@gmail.com>
2024-05-25 03:58:47 +0300

    .

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(morgan("dev"))
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")))
+// app.use(express.static(path.join(__dirname, "/frontend/dist")))
 
 app.use("/api/auth", authRoutes)
 app.use(

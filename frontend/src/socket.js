de6a93efb35ee46f2ffd54de67486d8d5c37d1a2
amanuel tegegne <amanuel0924@gmail.com>
2024-05-25 09:26:27 +0300

    change

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client"
 
-export const baseURL = "https://t-movie.onrender.com"
+// export const baseURL = "https://t-movie.onrender.com"
+export const baseURL = "http://localhost:4000"
 
 const socket = io(baseURL, {
   withCredentials: true,
